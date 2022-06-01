11ca74a8975fcc3a00de727bc8e7eed7fa748f29
Karl Telle <karl.wilhelm.telle@gmail.com>
2022-06-01 14:50:34 +0200

    fix x axis domain

--- app.js
+++ app.js
@@ -25,34 +25,17 @@ function mainCurve($elem, inputData) {
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-    var x = d3.scale.linear()
-        .range([0, width]);
-
-    var y = d3.scale.linear()
-        .range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    var svg = d3.select("body").append("svg")
+	var svg = d3.select("body").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+    var x = d3.scale.linear()
+        .range([0, width]);
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
+    var y = d3.scale.linear()
+        .range([height, 0]);
 
 	if (inputData === undefined) {
 		x.domain(d3.extent(data, function(d) {
@@ -76,6 +59,23 @@ function mainCurve($elem, inputData) {
 		x.domain([d3.min(xData) - 10, d3.max(xData) + 10]);
 		y.domain([0, 1]);
 	}
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
 
     function getData() {
         if (inputData === undefined) {
